37812549b5dd129346756dd64e7a160abe60f32f
jerkoLegion <jerome.potie@gmail.com>
2021-09-01 10:35:03 +0200

    Add token verification service

--- routes/technos.js
+++ routes/technos.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 const connexion = require('../conf');
 
-router.get('/', (req, res) => {
+const { verifyToken } = require('../services/token');
+
+router.get('/', verifyToken, (req, res) => {
   connexion.query('SELECT * from techno', (err, result) => {
     if (err) {
       return res.status('500').json({
--- services/token.js
+++ services/token.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 
+const secret = process.env.SECRET;
+
 const generateToken = (req, res) => {
   const token = jwt.sign(
     { id: req.id, pseudo: req.pseudo },
@@ -11,6 +13,24 @@ const generateToken = (req, res) => {
   res.status(200).send({ auth: true });
 };
 
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  console.log(authHeader);
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, secret, (err, result) => {
+      if (err) {
+        return res.status(403).json(err);
+      }
+      // req.user = result;
+      next();
+    });
+  } else {
+    return res.status(400).send('No token provided');
+  }
+};
+
 module.exports = {
-  generateToken
+  generateToken,
+  verifyToken
 };
