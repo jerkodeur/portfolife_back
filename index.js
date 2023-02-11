d8295db22c34e114aa60c6ab3f479e0211b1914c
jerkoLegion <jerome.potie@gmail.com>
2023-02-11 23:23:13 +0100

    modify the proxy

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const cors = require('cors');
-const proxy = require('express-http-proxy');
 const express = require('express');
 
 const app = express();
@@ -11,28 +10,12 @@ const { getUserRole } = require('./services/verify');
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/proxy', proxy('www.portfolife.herokuapp.com'));
-
 app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, DELETE, OPTIONS, DELETE'
-  );
 
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Headers', '*');
-  }
-
-  next();
-});
-
-app.all('*', cors(), getUserRole);
-app.use('/admins', cors(), routes.admins);
-app.use('/projects', cors(), routes.projects);
-app.use('/technos', cors(), routes.technos);
+app.all('*', getUserRole);
+app.use('/admins', routes.admins);
+app.use('/projects', routes.projects);
+app.use('/technos', routes.technos);
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
