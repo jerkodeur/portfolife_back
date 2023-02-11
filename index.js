7f5efcc869f606fbdafdcd613d1bf572a1314a3a
jerkoLegion <jerome.potie@gmail.com>
2023-02-11 21:40:32 +0100

    modify cors options

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const app = express();
 const routes = require('./routes/index');
 const { getUserRole } = require('./services/verify');
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader(
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header(
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, PATCH, DELETE, OPTIONS, DELETE'
   );
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
   if (req.method === 'OPTIONS') {
     res.setHeader(
       'Access-Control-Allow-Headers',
-      'X-Requested-With, content-type'
+      'Origin, X-Requested-With, Content-Type, Accept'
     );
   }
 
