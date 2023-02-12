e02e33534f86a3d622611858b598349a549a69ba
jerkoLegion <jerome.potie@gmail.com>
2023-02-13 00:49:33 +0100

    test connexion

--- services/verify.js
+++ services/verify.js
@@ -17,7 +17,7 @@ const verifyPassword = (req, res, next) => {
       }
       if (!result[0] || !bcrypt.compareSync(password, result[0].password)) {
         return res.status(401).json({
-          message: `Identifiants incorrects ${req.body.email} ${req.body.password}`
+          message: `Identifiants incorrects ${req}`
         });
       }
       req.id = result[0].id;
